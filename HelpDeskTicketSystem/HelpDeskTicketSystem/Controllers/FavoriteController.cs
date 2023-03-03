703e8765c7a93441d4f4f02db583e2de3821e650
Mike <mike.blackburn.541@gmail.com>
2023-03-03 14:22:50 -0500

    added angular api and finished database

---
+++ HelpDeskTicketSystem/HelpDeskTicketSystem/Controllers/FavoriteController.cs
@@ -0,0 +1,21 @@
+﻿using HelpDeskTicketSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskTicketSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteController : ControllerBase
+    {
+        FavoriteDbContext dbContext = new FavoriteDbContext();
+
+        //[HttpGet("Favorites")]
+        //public List<Favorite> GetFavorites()
+        //{
+        //    List<Favorite> Favorites = new List<Favorite>();
+        //    Favorites.Add(dbContext);
+        //    return dbContext.Favorites;
+        //}
+    }
+}
---
+++ HelpDeskTicketSystem/HelpDeskTicketSystem/Controllers/FavoriteDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpDeskTicketSystem.Controllers
+{
+    internal class FavoriteDbContext
+    {
+        public object Favorites { get; internal set; }
+    }
+}
